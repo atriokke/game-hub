c0553181985134cc714b8c25fc92055396ee0b83
Atriokke <atriokke@gmail.com>
2024-04-24 20:28:08 -0700

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,6 +13,7 @@ export interface Game {
     name: string;
     background_image: string;
     parent_platforms: {platform:Platform}[]
+    metacritic: number;
   }
   
   //this is is coming from the definition in the API
